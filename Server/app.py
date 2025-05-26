333007fb9a9558a6ae2b6d0a3978fe8f81d157a8
Guru Saran M <gurusaranm0025@gmail.com>
2025-05-26 13:48:00 +0530

    created endpoints to add engineers and tags and fixed some bugs

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 
 # local imports
-from db import get_session, Ticket, Status
+from db import get_session, Tickets, Status, Engineers, Tags
 from methods import ticket_to_json
 
 TICKETS_LIMIT = 5
@@ -12,17 +12,39 @@ session = get_session()
 @app.route('/new_ticket', methods=['POST'])
 def new_ticket():
     """
-        Create a new ticket
+        Create a new ticket.
+        
+        Incoming JSON Structure:
+            {
+                title: string,
+                description: string,
+                status: string,
+                created_by: string -> will change to user or engineer id,
+                engineer_ids: list[int] => list[engineers.id],
+                tag_ids: list[int] => list[tags.id],
+            }
     """
     if request.method == 'POST':
         try:
-            request_data = request.get_json()
+            data = request.get_json()
+            
+            if len(data["engineer_ids"])>0:
+                engineers = session.query(Engineers).filter(Engineers.id.in_(data["engineer_ids"])).all()
+            else:
+                engineers = []
 
-            ticket = Ticket(
-                title=request_data['title'], 
-                description=request_data['description'], 
-                status=Status(request_data['status']), 
-                created_by=request_data['created_by']
+            if len(data["tag_ids"])>0:            
+                tags = session.query(Tags).filter(Tags.id.in_(data["tag_ids"])).all()
+            else:
+                tags = []
+
+            ticket = Tickets(
+                title=data['title'], 
+                description=data['description'], 
+                status=Status(data['status']), 
+                created_by=data['created_by'],
+                engineers=engineers,
+                tags=tags
             )
             
             session.add(ticket)
@@ -45,17 +67,19 @@ def get_tickets(offset_val, filter_name, value):
         try:
             if offset_val == None:
                 offset_val = 0
-            if filter_name == None or value == None:
+            if filter_name == None or value == None: # rethink this approach for checking the if both values are present
                 filter_name, value = None, None
                 
             if filter_name == 'status' and len(value)>0:
-                tickets = session.query(Ticket).filter(Ticket.status==Status(value) and not Ticket.is_deleted).offset(offset_val).limit(TICKETS_LIMIT).all()
+                tickets = session.query(Tickets).filter(Tickets.status==Status(value) and Tickets.is_deleted==False).offset(offset_val).limit(TICKETS_LIMIT).all()
             elif filter_name == 'user' and len(value)>0:
-                tickets = session.query(Ticket).filter(Ticket.created_by==value and not Ticket.is_deleted).offset(offset_val).limit(TICKETS_LIMIT).all()
+                tickets = session.query(Tickets).filter(Tickets.created_by==value and Tickets.is_deleted==False).offset(offset_val).limit(TICKETS_LIMIT).all()
             else:                
-                tickets = session.query(Ticket).filter(not Ticket.is_deleted).offset(offset_val).limit(TICKETS_LIMIT).all()
+                tickets = session.query(Tickets).filter(Tickets.is_deleted==False).offset(offset_val).limit(TICKETS_LIMIT).all()
             
+            print(tickets[0])
             data = ticket_to_json(tickets)
+            # print(data)
             return data, 200
         except Exception as e:
             return jsonify({'error': str(e)}), 500
@@ -68,7 +92,7 @@ def handle_update(column_name, id, new_value):
     if request.method == 'POST':
         try:
             if column_name != None and id != None and new_value != None:
-                ticket = session.query(Ticket).filter(Ticket.id==id).first()
+                ticket = session.query(Tickets).filter(Tickets.id==id and Tickets.is_deleted==False).first()
 
                 if column_name == 'status':
                     ticket.status = Status(new_value)
@@ -87,11 +111,12 @@ def handle_update(column_name, id, new_value):
 def handle_delete(id, hard_delete):
     """
         Delete any ticket.
+        (Fixed this ==> This endpoint allows hard delete of soft deleted items)
     """
     if request.method == 'DELETE':
         try:
-            if id>0 :
-                ticket = session.query(Ticket).get(id)
+            # if id>0 :
+                ticket = session.query(Tickets).filter(Tickets.is_deleted==False).get(id)
                 
                 if hard_delete:
                     session.delete(ticket)
@@ -101,10 +126,36 @@ def handle_delete(id, hard_delete):
                 session.commit()
                 
                 return jsonify({'message': f'deleted ticket with id: {id}'}), 200
-            else:
-                return jsonify({'error': 'id is invalid'}), 400
+            # else:
+            #     return jsonify({'error': 'id is invalid'}), 400
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+@app.route("/add_engineer", methods=['POST'])
+def add_engineer():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            engineer = Engineers(name=data['name'], role=data["role"])
+            
+            session.add(engineer)
+            session.commit()
+            return jsonify({'message': 'added engineer successfully.'}), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+@app.route("/add_tag", methods=['POST'])
+def add_tag():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            tag = Tags(name=data["name"], description=data["description"])
+            
+            session.add(tag)
+            session.commit()
+            return jsonify({'message': 'added tag successfully.'}), 200
         except Exception as e:
             return jsonify({'error': str(e)}), 500
                 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
